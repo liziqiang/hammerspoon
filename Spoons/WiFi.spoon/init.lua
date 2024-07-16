1c68ba91a7ce5b2328ada88bbe9697879b6d5e51
Ziqiang Li <ziqiang.lee@gmail.com>
2024-07-16 11:32:54 +0800

    feat: 🎸 优化Wi-Fi代理检测

--- Spoons/WiFi.spoon/init.lua
+++ Spoons/WiFi.spoon/init.lua
@@ -13,6 +13,7 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 function obj:init()
     -- 需要自动启用代理的SSID列表
     self.ssidToEnableProxy = { 'bazinga' }
+    self.gateway = { 'Wi-Fi', 'USB 10/100/1000 LAN' }
     ssidChangedCallback()
     wifiWatcher = hs.wifi.watcher.new(ssidChangedCallback)
     wifiWatcher:start()
@@ -23,19 +24,23 @@ function ssidChangedCallback()
         ssidChangeTimer:stop()
     end
     ssidChangeTimer = hs.timer.doAfter(1, function ()
-        obj.ssid = hs.wifi.currentNetwork()
-        obj.succ, obj.output = hs.osascript.applescript('do shell script "networksetup -getwebproxy Wi-Fi"')
-        local isProxyEnabled = obj.succ and string.find(obj.output, 'Enabled: Yes') ~= nil
-        if hs.fnutils.contains(obj.ssidToEnableProxy, obj.ssid) then
+        local index = 0
+        local isProxyEnabled
+        local enabled = 'Enabled: Yes'
+        local ssid = hs.wifi.currentNetwork()
+        repeat
+           index = index + 1
+           local succ, output = hs.osascript.applescript('do shell script "networksetup -getwebproxy \\"' .. obj.gateway[index] .. '\\""')
+           isProxyEnabled = succ and string.find(output, enabled) ~= nil
+           print(index, output)
+        until( isProxyEnabled or index >= #obj.gateway )
+        if hs.fnutils.contains(obj.ssidToEnableProxy, ssid) then
             if not isProxyEnabled then
-                hs.eventtap.keyStroke({"cmd", "shift", "alt", "ctrl"}, "m")
-                -- hs.osascript.applescript('tell application "ClashX" to toggleProxy')
+                toggleProxy()
             end
-            -- handleBattery()
         else
             if isProxyEnabled then
-                hs.eventtap.keyStroke({"cmd", "shift", "alt", "ctrl"}, "m")
-                -- hs.osascript.applescript('tell application "ClashX" to toggleProxy')
+                toggleProxy()
             end
         end
     end)
@@ -52,6 +57,11 @@ function handleBattery()
         end)
         batteryWatcher:start()
     end
+end
+
+function toggleProxy()
+    hs.eventtap.keyStroke({"cmd", "shift", "alt", "ctrl"}, "m")
+    -- hs.osascript.applescript('tell application "ClashX" to toggleProxy')
 end
 
 return obj
--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ hs.alert.defaultStyle.strokeColor = {
 }
 
 -- 定义不需要加载的spoon，默认下载所有
-hspoon_list = {"USB", "BingDaily", "Weather", "WiFi"}
+hspoon_list = {"USB", "BingDaily", "Weather"}
 
 -- 加载Spoon
 for _, v in pairs(hs.spoons.list()) do
