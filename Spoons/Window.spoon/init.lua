95a76465c3df272c88f17464c97cd7e022ac1456
Ziqiang Li <ziqiang.lee@gmail.com>
2026-07-18 20:17:13 +0800

    feat: 调整启动项

--- Spoons/Window.spoon/init.lua
+++ Spoons/Window.spoon/init.lua
@@ -49,7 +49,7 @@ end
 -- 自定义窗口尺寸
 module.locate = function()
     local this = windowMeta.new()
-    local cell = hs.geometry(this.screenGrid.w * 2 / 16, this.screenGrid.h * 0 / 16, this.screenGrid.w * 12 / 16, this.screenGrid.h * 24 / 32)
+    local cell = hs.geometry(this.screenGrid.w * 1 / 16, this.screenGrid.h * 0 / 16, this.screenGrid.w * 14 / 16, this.screenGrid.h * 24 / 32)
     grid.set(this.window, cell, this.screen)
 end
 
